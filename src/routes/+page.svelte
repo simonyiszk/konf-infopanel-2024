9928cfe77c1bafc0a28c2830d7aca792bbdfdff9
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-12 17:53:57 +0100

    init sveltekit with tailwind, scss, changesets

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+	</ul>
+</nav>
+
+<style lang="scss">
+	nav {
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+
+		li {
+			display: inline-block;
+		}
+
+		a {
+			display: block;
+			padding: 6px 12px;
+			text-decoration: none;
+			color: green;
+		}
+	}
+</style>
