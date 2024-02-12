7306c5655a87762f17e579008fcd21b849c51071
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-12 23:18:33 +0100

    feat: implement countdown

---
+++ src/lib/components/countdown/KonfCountdown.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { DurationObjectUnits } from 'luxon';
+	import KonfCountdownElement from './KonfCountdownElement.svelte';
+
+	export let remaining: DurationObjectUnits & { done: boolean } = {
+		years: 0,
+		months: 0,
+		weeks: 0,
+		days: 0,
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
+		done: true
+	};
+</script>
+
+<div class="grid grid-cols-4 gap-4">
+	<KonfCountdownElement value={remaining?.days} label="nap" />
+	<KonfCountdownElement value={remaining?.hours} label="óra" />
+	<KonfCountdownElement value={remaining?.minutes} label="perc" />
+	<KonfCountdownElement value={remaining?.seconds} label="mp" />
+</div>
---
+++ src/lib/components/countdown/KonfCountdownElement.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let value: number | undefined;
+	export let label: string;
+</script>
+
+<div class="flex flex-col gap-2">
+	<p class="text-7xl font-bold text-center">{String(value).padStart(2, '0')}</p>
+	<p class="text-3xl text-center">{label}</p>
+</div>
