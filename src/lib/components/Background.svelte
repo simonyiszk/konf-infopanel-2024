72b84921addeb2e80ff20aed2cbe43086254d29e
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-12 21:16:49 +0100

    feat: add background component

---
+++ src/lib/components/Background.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let color: string = 'black';
+	export let image: string = '';
+</script>
+
+<div class="bg" style="--color:{color};background-image: url('{image}');">
+	<slot />
+</div>
+
+<style lang="scss">
+	.bg {
+		@apply size-full;
+		background-color: var(--color);
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		z-index: -1;
+	}
+</style>
