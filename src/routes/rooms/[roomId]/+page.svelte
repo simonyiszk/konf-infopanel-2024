4024cc668c45637f0cf87a2a8e1b704eef251fdb
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-12 22:27:26 +0100

    feat: create an example for tiles in [roomId] page

--- src/routes/rooms/[roomId]/+page.svelte
+++ src/routes/rooms/[roomId]/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Tile from '$lib/components/tile/Tile.svelte';
 
 	export let data: PageData;
 	const { presentations } = data;
 </script>
+
+<Tile let:Body>
+	<Body>
+		<h1 class="text-6xl text-white font-bold">{presentations[0].title}</h1>
+		<p class="text-4xl text-white font-medium">{presentations[0].room}</p>
+	</Body>
+</Tile>
