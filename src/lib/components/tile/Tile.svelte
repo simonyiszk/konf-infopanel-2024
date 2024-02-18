8c320939d5862cce405a9b044ffe6555a9e41c85
AndrisBorbas <andris.borbas@gmail.com>
2024-02-18 19:23:09 +0100

    get-rotated-idiot

--- src/lib/components/tile/Tile.svelte
+++ src/lib/components/tile/Tile.svelte
@@ -15,20 +15,40 @@
 </div>
 
 <style lang="scss">
+	@property --rotation {
+		syntax: '<angle>';
+		inherits: false;
+		initial-value: 150deg;
+	}
+
+	@keyframes -global-sbin {
+		from {
+			--rotation: 150deg;
+		}
+		to {
+			--rotation: 510deg;
+		}
+	}
+
 	.tilebase {
 		--rotation: 150deg;
 		--border-rounding: 30px;
 
 		@apply overflow-hidden relative p-[1.5px];
 
-		border-radius: var(--border-rounding);
-		background: rgb(255, 255, 255);
-		background: linear-gradient(
-			calc(var(--rotation)),
-			rgba(128, 128, 128, 1) 0%,
-			rgba(0, 212, 255, 0) 20%
-		);
-		background-origin: border-box;
-		background-clip: padding-box, border-box;
+		&::before {
+			@apply absolute inset-0;
+			content: '';
+			border-radius: var(--border-rounding);
+			background: rgb(255, 255, 255);
+			background: linear-gradient(
+				calc(var(--rotation)),
+				rgba(128, 128, 128, 1) 0%,
+				rgba(0, 212, 255, 0) 20%
+			);
+			background-origin: border-box;
+			background-clip: padding-box, border-box;
+			animation: sbin 4s 0ms infinite linear;
+		}
 	}
 </style>
