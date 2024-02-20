c4c3f8abd2c44f6a423fb257f523ece96ce8013d
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-20 17:58:29 +0100

    feat: implement playbackbar

---
+++ src/lib/components/playback/Bar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let currentTime: string;
+	export let progress: number;
+</script>
+
+<div
+	style="--progress-bar-transform:{progress}%;"
+	class="bg-white w-full h-4 rounded-2xl overflow-hidden"
+>
+	<div class="progress"></div>
+</div>
+
+<style lang="scss">
+	.progress {
+		transform: translateX(calc(-100% + var(--progress-bar-transform)));
+		@apply bg-yellow-300 h-full w-full;
+	}
+</style>
