f9ede837f42a34b1dfa2f0cc64a425f49fe193a3
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-20 17:59:51 +0100

    feat: create base room presentation page

--- src/routes/rooms/[roomId]/+page.svelte
+++ src/routes/rooms/[roomId]/+page.svelte
@@ -3,6 +3,8 @@
 	import Tile from '$lib/components/tile/Tile.svelte';
 	import { error } from '@sveltejs/kit';
 
+	import PresentationRoom from '$lib/components/presentation/room';
+
 	export let data: PageData;
 	const { presentations } = data;
 
@@ -12,35 +14,19 @@
 		error(404, 'No presentations found');
 	}
 
-	const selected = presentations[1];
+	const selected = presentations[0];
+	const nextPresentation = presentations.at(presentations.indexOf(selected) + 1) ?? null;
 
-	const { title, room, presenter } = selected;
+	const { presenter } = selected;
 </script>
 
 <Tile let:Body class="size-full">
 	<Body class="size-full">
-		<div class="grid grid-cols-3 gap-8">
-			<div class="col-span-2">
-				<h1 class="text-7xl text-white font-bold">{title}</h1>
-				<p class="text-4xl text-white font-medium">{room}</p>
-				<p class="text-3xl">{selected.description}</p>
-			</div>
-			<div class="col-span-1 flex flex-col items-center gap-4">
-				<img class="relative object-cover rounded-3xl" src={selected.presenter.pictureUrl} alt="" />
-				<span class="text-4xl font-bold">{presenter.name}</span>
-				<div class="text-2xl">
-					{#if presenter.rank}
-						<span>{presenter.rank}</span>
-					{/if}
-					{#if presenter.rank && presenter.company}
-						<span> @ </span>
-					{/if}
-					{#if presenter.company}
-						<span>{presenter.company.name}</span>
-					{/if}
-				</div>
+		<div class="flex flex-col justify-center items-center h-full">
+			<div class="grid grid-cols-2 gap-16">
+				<PresentationRoom.LeftSide presentation={selected} {nextPresentation} />
+				<PresentationRoom.RightSide {presenter} />
 			</div>
 		</div>
-		<div class="bg-red-400">timeprogess here</div>
 	</Body>
 </Tile>
