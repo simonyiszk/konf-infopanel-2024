442fbd8d58752ef417863767ec8dd1b5c93e9388
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-20 17:57:58 +0100

    feat: create dynamic size title component

---
+++ src/lib/components/DynamicSizeTitle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let text: string;
+	export let options = {
+		baseSize: 40,
+		baseLength: 60,
+		overLimitMultiplier: 10,
+		baseSizeMultiplier: 10
+	};
+
+	const getFontSize = (textLength: number) => {
+		const baseSize = options.baseSize;
+		const baseLength = options.baseLength;
+		if (textLength > baseLength) {
+			return `${baseSize * (baseLength / textLength / options.overLimitMultiplier)}vw`;
+		}
+		return `${baseSize / options.baseSizeMultiplier}vw`;
+	};
+</script>
+
+<slot size={getFontSize(text.length)} />
