37faa05cac69ae66be8952da295af8edaa8ca415
Miklos Daniel <daniel.miklos0@gmail.com>
2024-03-09 12:27:55 +0100

    modified layout for better usablitiy

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -2,7 +2,6 @@
 	import type { PageData } from './$types';
 
 	import Carousel from '$lib/components/carousel-diy';
-	import SiemaWrapper from '$lib/components/carousel-diy/SiemaWrapper.svelte';
 	import { dev } from '$app/environment';
 
 	export let data: PageData;
@@ -16,56 +15,68 @@
 	if (bosch) {
 		ib028.unshift(bosch);
 	}
+
+	const c = ib028.map((p, i) => [p, ib025[i]]);
 </script>
 
-<div class="flex flex-col gap-4 justify-around h-full">
-	{#each [ib028, ib025] as pres}
-		<Carousel.SiemaWrapper autoplay={5 * 1000} perPage={2} dots={false} draggable={dev}>
-			{#each pres as p}
-				<div class="grid grid-cols-3 gap-4">
-					<div class="cols-span-1">
-						<img
-							class="size-full object-cover rounded-2xl"
-							src={p.presenter.pictureUrl}
-							alt={p.presenter.name}
-						/>
+<div class="flex flex-col justify-center items-center h-full">
+	<div class="grid grid-cols-12 w-full">
+		<div class="col-span-11">
+			<Carousel.SiemaWrapper autoplay={3 * 1000} perPage={2} dots={false} draggable={dev}>
+				{#each c as pre}
+					<div class="flex flex-col gap-8">
+						{#each pre as p}
+							<div class="row-span-1 grid grid-cols-3 gap-4 p-2">
+								<div>
+									<img
+										class="size-full object-cover rounded-2xl"
+										src={p.presenter.pictureUrl}
+										alt={p.presenter.name}
+									/>
+								</div>
+								<div class="col-span-2">
+									<h1
+										class:text-3xl={p.title.length > 70}
+										class:text-4xl={p.title.length > 45}
+										class:text-5xl={p.title.length <= 45}
+										class="font-medium"
+									>
+										{p.title}
+									</h1>
+									<p class="text-3xl">{p.presenter.name}</p>
+									{#if p.presenter.company}
+										<p class="text-2xl">{p.presenter.company.name}</p>
+									{/if}
+								</div>
+							</div>
+						{/each}
+						<div class="p-2">
+							<div class="p-8 flex flex-row justify-between h-fit bg-white/10 rounded-xl">
+								{#each [pre[1].startTime, pre[1].endTime] as t}
+									<p class="text-5xl font-bold">
+										{t.toLocaleTimeString('hu', {
+											hour: '2-digit',
+											minute: '2-digit'
+										})}
+									</p>
+								{/each}
+							</div>
+						</div>
 					</div>
-					<div class="col-span-2">
-						<h1
-							class:text-4xl={p.title.length > 45}
-							class:text-5xl={p.title.length <= 45}
-							class="font-medium"
-						>
-							{p.title}
-						</h1>
-						<p class="text-3xl">{p.presenter.name}</p>
-						{#if p.presenter.company}
-							<p class="text-2xl">{p.presenter.company.name}</p>
-						{/if}
-					</div>
-				</div>
+				{/each}
+			</Carousel.SiemaWrapper>
+		</div>
+		<div
+			class="grid grid-cols-5 font-bold col-span-1 w-full justify-between gap-8"
+			style="writing-mode: vertical-rl;"
+		>
+			{#each ['IB028', 'IB025'] as room}
+				<p
+					class="rounded-xl bg-white/10 col-span-2 text-6xl text-center flex justify-center items-center"
+				>
+					{room}
+				</p>
 			{/each}
-		</Carousel.SiemaWrapper>
-	{/each}
-
-	<Carousel.SiemaWrapper autoplay={5 * 1000} perPage={2} dots={false} draggable={dev}>
-		{#each ib025 as p}
-			<div class=" p-2 text-6xl font-bold">
-				<div class="p-8 flex flex-row justify-around bg-white/10 rounded-xl">
-					<p>
-						{p.startTime.toLocaleTimeString('hu', {
-							hour: '2-digit',
-							minute: '2-digit'
-						})}
-					</p>
-					<p>
-						{p.endTime.toLocaleTimeString('hu', {
-							hour: '2-digit',
-							minute: '2-digit'
-						})}
-					</p>
-				</div>
-			</div>
-		{/each}
-	</Carousel.SiemaWrapper>
+		</div>
+	</div>
 </div>
